767c51f8e0f598a90ae97f99cf6e63e9c0a122be
Manish Patel <79135089+Manish0037@users.noreply.github.com>
2026-05-08 14:22:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+﻿const header = document.querySelector(".site-header");
+
+const setHeaderState = () => {
+  header.dataset.elevated = String(window.scrollY > 24);
+};
+
+window.addEventListener("scroll", setHeaderState, { passive: true });
+setHeaderState();
